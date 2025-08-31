444b17fef571f69b497bc2631c86393da0ae1c08
Joshua Hyde <jrh3k5@gmail.com>
2025-08-31 13:45:26 +0000

    Centralize currency formatting, add debug mode (#57)

---
+++ currency/format.go
@@ -0,0 +1,28 @@
+package currency
+
+import (
+	"fmt"
+	"math"
+)
+
+// FormatCents formats the given expression of USD in cents to a dollar-and-cents string.
+func FormatCents(cents int) string {
+	remainderCents := cents % 100
+	dollars := (cents - remainderCents) / 100
+
+	return FormatDollarsAndCents(dollars, remainderCents)
+}
+
+// FormatDollarsAndCents formats the given USD dollars and cents to a dollar-and-cents string.
+func FormatDollarsAndCents(dollars int, cents int) string {
+	absoluteDollars := int(math.Abs(float64(dollars)))
+	absoluteCents := int(math.Abs(float64(cents)))
+
+	formattedDollars := fmt.Sprintf("$%d.%02d", absoluteDollars, absoluteCents)
+
+	if dollars < 0 || cents < 0 {
+		return "-" + formattedDollars
+	}
+
+	return formattedDollars
+}
